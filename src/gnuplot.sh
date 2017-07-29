d37c844955b45138111ad38956b28c5394dad843
psato <forhighlow@gmail.com>
2017-07-29 20:01:16 +0900

    fix gnuplot

--- src/gnuplot.sh
+++ src/gnuplot.sh
@@ -1,5 +1,6 @@
 #.shでないファイルを入ってから渡すときgnuplot>load 'fuga’とする．                                                                        
 #$file="result/100-100-1000000.csv"
+#ファイル名はplot"result/hoge/mergesort\_"が無い方がいい
 
 #空行抜き
 gnuplot << EOF
@@ -7,13 +8,11 @@ gnuplot << EOF
    set xlabel 'size'
    set ylabel 'time'
    set style fill solid border lc rgb "black"
-   plot 'result/mergesort\_100-100-1000000.csv' using 1:2 with lines lw 2  title "mergesort.go"#,\
-        # 'result/mergesort\_pointer\_100-100-1000000.csv' using 1:2 with lines lw 2  title "mergesort.go",\
-        # 'result/pmergesort\_pointer\_buildin\_100-100-1000000.csv' using 1:2 with lines lw 2 title "pmergesort_pointer_buildin.go",\
-        # 'result/pmergesort\_pointer\_bubble\_100-100-1000000.csv' using 1:2 with lines lw 2 title "pmergesort_pointer_bubble.go"
-   set output 'result.svg'
-   replot
-   set output
+   plot 'result/mergesort_100-100-1000000.csv' using 1:2 with lines lw 2  title "mergesort.go",\
+        'result/mergesort_pointer_100-100-1000000.csv' using 1:2 with lines lw 2  title "mergesort.go",\
+        'result/pmergesort_pointer_buildin_100-100-1000000.csv' using 1:2 with lines lw 2 title "pmergesort_pointer_buildin.go",\
+        'result/pmergesort_pointer_bubble_100-100-1000000.csv' using 1:2 with lines lw 2 title "pmergesort_pointer_bubble.go"
+#   set output 'result.svg'
    set terminal png
    set output "result.png"
    replot
